157a38f09ddc3d691a3d786a91e7c71d6ead67b4
i3ym <deniso99@mail.ru>
2020-03-20 21:31:30 +0700

    Добавил красоты колёсам

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -8,6 +8,7 @@ namespace DeloG
         public WheelCollider WheelCollider => Collider;
         public bool Rotatable => IsRotatable;
 
+        [SerializeField] MeshRenderer Mesh = null;
         [SerializeField] bool IsRotatable = false;
         WheelCollider Collider;
         float HorizontalAngle = 0f;
@@ -21,14 +22,20 @@ namespace DeloG
         {
             Collider.brakeTorque = brakeTorque;
             Collider.motorTorque = motorTorque;
-            transform.Rotate(transform.right, Collider.rpm * 360 / 60 * Time.fixedDeltaTime, Space.World);
+            Mesh.transform.Rotate(Mesh.transform.right, Collider.rpm * 360 / 60 * Time.fixedDeltaTime, Space.World);
 
             if (Rotatable)
             {
                 Collider.steerAngle = horizontalAngle;
-                transform.Rotate(transform.parent.up, -(HorizontalAngle - horizontalAngle), Space.World);
+                Mesh.transform.Rotate(transform.parent.up, -(HorizontalAngle - horizontalAngle), Space.World);
                 HorizontalAngle = horizontalAngle;
-            }
+            }//, LayerMask.GetMask("Default")
+
+            var center = transform.TransformPoint(Collider.center);
+            var raycast = Physics.Raycast(center, -transform.parent.up, out var hit, Collider.suspensionDistance + Collider.radius);
+
+            if (raycast) Mesh.transform.position = hit.point + (transform.parent.up * Collider.radius);
+            else Mesh.transform.position = center - (transform.parent.up * Collider.suspensionDistance);
         }
     }
 }
