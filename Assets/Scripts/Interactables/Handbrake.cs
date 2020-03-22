44b2c6a631a774c7fbbb5f56e9e0a661f5d1db25
i3ym <deniso99@mail.ru>
2020-03-22 15:05:23 +0700

    Добавил анимацию ручника

--- Assets/Scripts/Interactables/Handbrake.cs
+++ Assets/Scripts/Interactables/Handbrake.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 namespace DeloG.Interactables
 {
     public class Handbrake : Lever
     {
-        const float HandbrakeAngle = 30; // угол поворота ручника
+        const float HandbrakeAngle = 80; // угол поворота ручника
 
+        Quaternion StartRotation, ToRotation;
         [SerializeField] Car Car = null;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            StartRotation = transform.localRotation;
+            ToRotation = Quaternion.Euler(-HandbrakeAngle, 0, 0);
+        }
 
         protected override void OnToggle()
         {
             Car.IsHandbrakeOn = true;
-            transform.Rotate(transform.right, -HandbrakeAngle, Space.World);
+            StartCoroutine(Animator.RotateTo(transform, ToRotation, .7f, Easing.OutQuad));
         }
         protected override void OnUntoggle()
         {
             Car.IsHandbrakeOn = false;
-            transform.Rotate(transform.right, HandbrakeAngle, Space.World);
+            StartCoroutine(Animator.RotateTo(transform, StartRotation, .7f, Easing.OutQuad));
         }
     }
 }
