d1d55d461a7d2a9c3984e2b866effd780aa5ef2b
i3ym <deniso99@mail.ru>
2020-03-27 16:41:24 +0700

    Ускорил анимацию ручника

--- Assets/Scripts/Interactables/Handbrake.cs
+++ Assets/Scripts/Interactables/Handbrake.cs
@@ -9,7 +9,7 @@ namespace DeloG.Interactables
         protected override Quaternion ToggledRotation { get; } = Quaternion.Euler(-80, 0, 0);
         protected override Func<float, float> ToggleEasing { get; } = Easing.OutQuad;
         protected override Func<float, float> UntoggleEasing { get; } = Easing.OutQuad;
-        protected override float AnimTime { get; } = .7f;
+        protected override float AnimTime { get; } = .4f;
 
         protected override void OnToggle(bool toggled) => Car.IsHandbrakeOn = toggled;
     }
