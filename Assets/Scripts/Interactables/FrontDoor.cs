ae93054019627dd584539a4dab2a375144806ba5
i3ym <deniso99@mail.ru>
2020-05-15 08:43:11 +0700

    Объединил скрипты дверей в один

---
+++ Assets/Scripts/Interactables/FrontDoor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+namespace DeloG.Interactables
+{
+    public class FrontDoor : Door
+    {
+        [SerializeField] Side DoorSide;
+        protected const float MaxAngle = 80; // угол открытой двери
+
+        public override IEnumerator OpenAnimation()
+        {
+            var maxAngle =
+                DoorSide == Side.Left ? MaxAngle
+                : DoorSide == Side.Right ? -MaxAngle
+                : throw new ArgumentException();
+
+            return Animator.RotateToLocal(transform, Quaternion.Euler(0, maxAngle, 0), 1f, Easing.OutCubic);
+        }
+        public override IEnumerator CloseAnimation() =>
+            Animator.RotateToLocal(transform, Quaternion.identity, 1f, Easing.OutCubic);
+
+
+        enum Side : byte { Left, Right }
+    }
+}
